0f7a7c6a78f138b8f252066bca19e93d99a56c31
Han Maokun <hanmaokun@gmail.com>
2018-03-23 17:49:09 +0800

    Save img stream content as file first.

--- dlib/dlib/fhog_svm_detector.cpp
+++ dlib/dlib/fhog_svm_detector.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <strstream>
+#include <stdio.h>
 
 #include "../dlib/svm_threaded.h"
 #include "../dlib/array.h"
@@ -11,6 +12,7 @@
 #include "../dlib/image_keypoint.h"
 #include "../dlib/image_processing.h"
 #include "../dlib/data_io.h"
+#include "../dlib/image_io.h"
 
 #include "fhog_svm_detector.h"
 
@@ -50,29 +52,33 @@ bool isfile(const char* str){
 }
 
 const int* fhog_svm_det(const char* img_path, const char* model_path, int length){
-    dlib::array<array2d<unsigned char> > images;
-
-    images.resize(1);
-
-    array2d<unsigned char> sizeImg(450, 500);
+    //array2d<unsigned char> sizeImg(450, 500);
+    int L_tmpname = 200;
     array2d<unsigned char> loadedImg;
     if(isfile(img_path)){
         cout << "is image." << endl;
         load_image(loadedImg, img_path);
 
-        resize_image(loadedImg, sizeImg);
+        //resize_image(loadedImg, sizeImg);
     }else{
         char *img_content = (char *)img_path;
-        imemstream in(img_content, length);
-        load_bmp(images[0], in);
-        resize_image(images[0], sizeImg);
+        char name_buffer[L_tmpname];
+        tmpnam(name_buffer);
+        FILE *tempfile = fopen(name_buffer, "wb");
+        fwrite(img_content, sizeof(char), length, tempfile);
+        fflush(tempfile);
+        //imemstream in(img_content, length);
+        //load_bmp(images[0], in);
+        //resize_image(images[0], sizeImg);
+        load_image(loadedImg, name_buffer);
+        remove(name_buffer);
     }
 
     typedef scan_fhog_pyramid<pyramid_down<6> > image_scanner_type;
     object_detector<image_scanner_type> detector;
     deserialize(model_path) >> detector;
 
-    const std::vector<rectangle> rects = detector(sizeImg);
+    const std::vector<rectangle> rects = detector(loadedImg);
     cout << "Number of detections: "<< rects.size() << endl;
 
     int* ret_rect = new int[4];
@@ -95,37 +101,3 @@ const int* fhog_svm_det(const char* img_path, const char* model_path, int length
 int test_dlib_add(int x, int y) {
   return x+y;
 }
-
-/* test. */
-// int main()
-// {  
-//     try
-//     {
-//         image_window win;
-//         {
-//             // Run the detector on images
-//             const std::vector<rectangle> rects = fhog_svm_det("/home/nlp/bigsur/data/data_ssd_id_train_resized/03903.jpg");
-//             cout << "Number of detections: "<< rects.size() << endl;
-
-//             // Put the image and detections into the window.
-//             win.clear_overlay();
-//             dlib::array<array2d<unsigned char> > images_test;
-
-//             images_test.resize(1);
-//             load_image(images_test[0], "/home/nlp/bigsur/data/data_ssd_id_train_resized/03903.jpg");
-//             win.set_image(images_test[0]);
-//             win.add_overlay(rects, rgb_pixel(255,0,0));
-
-//             cout << "Hit enter to see the next image.";
-//             cin.get();
-//         }
-
-//     }
-//     catch (exception& e)
-//     {
-//         cout << "\nexception thrown!" << endl;
-//         cout << e.what() << endl;
-//     }
-// }
-
-// ----------------------------------------------------------------------------------------
